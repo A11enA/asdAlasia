8a71eb2e142a2b89a9c21e350e285b786f82b239
A11enA <alasiakallen@gmail.com>
2024-09-26 23:45:26 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -58,33 +58,47 @@ $(document).ready(function () {
       currentShape.goodBehavior = "spin"
     } 
   }
-
+  
   // TODO 3-a: add a function that handles the static display type
   
+  function handleStatic(data){
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
-
+  function handleGood(color,shape,repeat) {
+    setBackgroundWithSimple(color,shape,repeat);
+    animationDetails.displayType = 2
+  }
+  function handleBad(data,repeat) {
+    repeat++;
+    setBackgroundWithMixed(data,repeat);
+    animationDetails.displayType = 3;
+  }
   // TODO 5-a: add a function that handles the bad display type
   
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
   /////////////////////////////////////////////////
-
+console.log(currentIndex)
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color,currentShape.shape,currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape,repeat);
   }
 
   /////////////////////////////////////////////////
